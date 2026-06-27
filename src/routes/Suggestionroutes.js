f7251359ec082fd3b1ed1cec85c146dc733f874f
Moundirbechikh <moundirbechikh03@gmail.com>
2026-06-27 22:47:42 +0100

    Worldwide

--- src/routes/Suggestionroutes.js
+++ src/routes/Suggestionroutes.js
@@ -1,6 +1,6 @@
 const express    = require('express');
 const router     = express.Router();
-const auth       = require('../middleware/auth');
+const auth       = require('../middleware/authMiddleware');
 const ctrl       = require('../controllers/Suggestioncontroller');
 
 // Toutes les routes nécessitent une authentification
